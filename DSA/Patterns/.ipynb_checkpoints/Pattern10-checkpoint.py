b1c2480b0398dfd5269b7f0bead130e89174e48b
Rohan das <rd555217@gmail.com>
2024-11-15 21:19:37 +0530

    Delete DSA/Patterns/.ipynb_checkpoints directory

--- DSA/Patterns/.ipynb_checkpoints/Pattern10-checkpoint.py
+++
@@ -1,9 +0,0 @@
-def pattern2(num):
-    for i in range(1,num+1):
-        print("* "*i)
-    for i in range(num-1,0,-1):
-        print("* "*i)
-
-if __name__ == "__main__":
-    inp = int(input())
-    pattern2(inp)
